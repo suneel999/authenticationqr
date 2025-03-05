c8a2c43c2e8c8967100b4d1c699b00b02848a869
suneel999 <129484539+suneel999@users.noreply.github.com>
2025-03-05 15:59:32 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):  # Inherit from UserMixin
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
     qr_logs = db.relationship('QRLog', backref='user', lazy=True)
 
     def __repr__(self):
